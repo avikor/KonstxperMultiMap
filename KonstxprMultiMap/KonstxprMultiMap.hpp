28e12dd13af0624068cbfcab2499850ff172ac9b
Avi Korzac <avikor13@gmail.com>
2022-12-31 13:30:03 +0200

    Add project files.

---
+++ KonstxprMultiMap/KonstxprMultiMap.hpp
@@ -0,0 +1,141 @@
+﻿#include <array>
+#include <optional>
+#include <algorithm>
+#include <ranges>
+
+
+template<typename T>
+concept HasLessThan = requires(T a, T b) {
+    { a < b } -> std::convertible_to<bool>;
+};
+
+template<typename Key>
+concept KeyType = std::equality_comparable<Key> && HasLessThan<Key>;
+
+
+template <KeyType Key, typename Value, std::size_t SIZE, typename Comp = std::less<std::pair<Key, Value>>>
+class KonstxperMultiMap
+{
+public:
+    explicit constexpr KonstxperMultiMap() = delete;
+
+    template<typename... KVs>
+    explicit constexpr KonstxperMultiMap(KVs&&... kvs) noexcept;
+
+    [[nodiscard]] consteval std::size_t size() const noexcept;
+
+    [[nodiscard]] constexpr std::optional<Value> at(const Key& key) const noexcept;
+
+    [[nodiscard]] constexpr bool contains(const Key& key) const noexcept;
+    
+    [[nodiscard]] constexpr std::optional<Value> lowerBound(const Key& key) const noexcept;
+
+    [[nodiscard]] constexpr std::optional<Value> upperBound(const Key& key) const noexcept;
+
+    [[nodiscard]] constexpr auto equalRange(const Key& key) const noexcept;
+
+    [[nodiscard]] constexpr std::size_t count(const Key& key) const noexcept;
+
+private:
+    std::array<std::pair<Key, Value>, SIZE> m_arr;
+
+    [[nodiscard]] constexpr std::size_t find_first_of(const Key& key) const noexcept;
+    [[nodiscard]] constexpr std::size_t find_last_of(const Key& key) const noexcept;
+};
+
+template <KeyType Key, typename Value, std::size_t SIZE, typename Comp>
+template<typename... KVs>
+constexpr KonstxperMultiMap<Key, Value, SIZE, Comp>::KonstxperMultiMap(KVs&&... kvs) noexcept
+    : m_arr{ kvs... }
+{
+    std::ranges::sort(m_arr, Comp{});
+}
+
+template <KeyType Key, typename Value, std::size_t SIZE, typename Comp>
+constexpr std::size_t KonstxperMultiMap<Key, Value, SIZE, Comp>::find_first_of(const Key& key) const noexcept
+{
+    // for small contiguous containers linear search might be faster than binary search
+    // so throughout this class we'll use linear search rather than binary search
+    
+    auto it{ std::cbegin(m_arr) };
+    for (; it != std::cend(m_arr); ++it)
+    {
+        if (it->first == key)
+        {
+            return it - std::cbegin(m_arr);
+        }
+    }
+
+    return SIZE;
+}
+
+template <KeyType Key, typename Value, std::size_t SIZE, typename Comp>
+constexpr std::size_t KonstxperMultiMap<Key, Value, SIZE, Comp>::find_last_of(const Key& key) const noexcept
+{
+    auto it{ std::crbegin(m_arr) };
+    for (; it != std::crend(m_arr); ++it)
+    {
+        if (it->first == key)
+        {
+            return std::crend(m_arr) - it;
+        }
+    }
+
+    return SIZE;
+}
+
+template <KeyType Key, typename Value, std::size_t SIZE, typename Comp>
+consteval std::size_t KonstxperMultiMap<Key, Value, SIZE, Comp>::size() const noexcept
+{
+    return SIZE;
+}
+
+template <KeyType Key, typename Value, std::size_t SIZE, typename Comp>
+constexpr std::optional<Value> KonstxperMultiMap<Key, Value, SIZE, Comp>::at(const Key& key) const noexcept
+{
+    if (find_first_of(key) != SIZE)
+    {
+        return { m_arr[find_first_of(key)].second };
+    }
+    else
+    {
+        return { };
+    }
+}
+
+template <KeyType Key, typename Value, std::size_t SIZE, typename Comp>
+constexpr bool KonstxperMultiMap<Key, Value, SIZE, Comp>::contains(const Key& key) const noexcept
+{
+    return at(key).has_value();
+}
+
+template <KeyType Key, typename Value, std::size_t SIZE, typename Comp>
+constexpr std::optional<Value> KonstxperMultiMap<Key, Value, SIZE, Comp>::lowerBound(const Key& key) const noexcept
+{
+    return at(key);
+}
+
+template <KeyType Key, typename Value, std::size_t SIZE, typename Comp>
+constexpr std::optional<Value> KonstxperMultiMap<Key, Value, SIZE, Comp>::upperBound(const Key& key) const noexcept
+{
+    if (find_last_of(key) != SIZE)
+    {
+        return { m_arr[find_last_of(key)].second };
+    }
+    else
+    {
+        return { };
+    }
+}
+
+template <KeyType Key, typename Value, std::size_t SIZE, typename Comp>
+constexpr auto KonstxperMultiMap<Key, Value, SIZE, Comp>::equalRange(const Key& key) const noexcept
+{
+    return std::ranges::subrange(m_arr.cbegin() + find_first_of(key), m_arr.cbegin() + find_last_of(key)) | std::views::values;
+}
+
+template <KeyType Key, typename Value, std::size_t SIZE, typename Comp>
+constexpr std::size_t KonstxperMultiMap<Key, Value, SIZE, Comp>::count(const Key& key) const noexcept
+{
+    return equalRange(key).size();
+}
---
+++ KonstxprMultiMap/KonstxprMultiMapTests.cpp
@@ -0,0 +1,109 @@
+#include "KonstxprMultiMap.hpp"
+
+#include <cassert>
+
+consteval void test_size_at_contains_methods()
+{
+    constexpr KonstxperMultiMap<char, int, 3U> kMultiMap{ 
+        std::pair<char, int>{ 'b' , 1 },
+        std::pair<char, int>{ 'a', 0 },
+        std::pair<char, int>{ 'c', 2 }
+    };
+
+	static_assert(kMultiMap.size() == 3U);
+
+    constexpr std::optional<int> aVal = kMultiMap.at('a');
+    static_assert(*aVal == 0);
+
+    constexpr std::optional<int> bVal = kMultiMap.at('b');
+    static_assert(*bVal == 1);
+
+    constexpr std::optional<int> cVal = kMultiMap.at('c');
+    static_assert(*cVal == 2);
+
+    constexpr std::optional<int> dVal = kMultiMap.at('d');
+    static_assert(!dVal.has_value());
+
+    static_assert(kMultiMap.contains('a'));
+    static_assert(kMultiMap.contains('b'));
+    static_assert(kMultiMap.contains('c'));
+    static_assert(!kMultiMap.contains('d'));
+}
+
+consteval void test_lower_upper_equal_range_methods()
+{
+    constexpr KonstxperMultiMap<char, int, 7U> kMultiMap{
+        std::pair<char, int>{ 'a', 0 },
+        std::pair<char, int>{ 'a', 1 },
+        std::pair<char, int>{ 'b', 2 },
+        std::pair<char, int>{ 'b', 3 },
+        std::pair<char, int>{ 'b', 4 },
+        std::pair<char, int>{ 'c', 5 },
+        std::pair<char, int>{ 'd', 6 }
+    };
+
+    static_assert(*kMultiMap.lowerBound('a') == 0U);
+    static_assert(*kMultiMap.upperBound('a') == 2U);
+
+    static_assert(*kMultiMap.lowerBound('b') == 2U);
+    static_assert(*kMultiMap.upperBound('b') == 5U);
+
+    static_assert(*kMultiMap.lowerBound('c') == 5U);
+    static_assert(*kMultiMap.upperBound('c') == 6U);
+
+    static_assert(*kMultiMap.lowerBound('d') == 6U);
+    static_assert(!kMultiMap.upperBound('d').has_value());
+
+    static_assert(!kMultiMap.lowerBound('g').has_value());
+    static_assert(!kMultiMap.upperBound('g').has_value());
+}
+
+constexpr void test_equal_range_and_count_methods()
+{
+    constexpr KonstxperMultiMap<char, int, 7U> kMultiMap{
+        std::pair<char, int>{ 'b', 0 },
+        std::pair<char, int>{ 'a', -1 },
+        std::pair<char, int>{ 'b', 1 },
+        std::pair<char, int>{ 'c', -1 },
+        std::pair<char, int>{ 'b', 2 },
+        std::pair<char, int>{ 'd', -1 },
+        std::pair<char, int>{ 'b', 3 }
+    };
+
+    auto bRange = kMultiMap.equalRange('b');
+    int bExpectedVal{ 0 };
+    for (int i : bRange)
+    {
+        assert(i == bExpectedVal);
+        ++bExpectedVal;
+    }
+    assert(bExpectedVal == 4);
+    assert(kMultiMap.count('b') == 4U);
+
+
+    auto dRange = kMultiMap.equalRange('d');
+    int dExpectedVal{ -1 };
+    for (int i : dRange)
+    {
+        assert(i == dExpectedVal);
+        ++dExpectedVal;
+    }
+    assert(dExpectedVal == 0);
+    assert(kMultiMap.count('d') == 1U);
+
+
+    auto gRange = kMultiMap.equalRange('g');
+    assert(gRange.empty());
+    assert(kMultiMap.count('g') == 0U);
+}
+
+int main()
+{
+    test_size_at_contains_methods();
+
+    test_lower_upper_equal_range_methods();
+
+    test_equal_range_and_count_methods();
+
+	return EXIT_SUCCESS;
+}
